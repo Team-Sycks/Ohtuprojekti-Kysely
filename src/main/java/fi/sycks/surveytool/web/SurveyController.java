3c0c4666fca1d65b022d2f88b29f7203be09a216
Santtu Hyvärinen <santtu.hyvarinen@kuori.tech>
2019-10-29 15:25:48 +0200

    Added REST for kysely & kysymys

--- src/main/java/fi/sycks/surveytool/web/SurveyController.java
+++ src/main/java/fi/sycks/surveytool/web/SurveyController.java
@@ -1,18 +1,40 @@
 package fi.sycks.surveytool.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import fi.sycks.surveytool.domain.Kysely;
+import fi.sycks.surveytool.domain.Kysymys;
+import fi.sycks.surveytool.interfaces.KyselyRepository;
+import fi.sycks.surveytool.interfaces.KysymysRepository;
+
 @Controller
 @ResponseBody
-// test
 public class SurveyController {
-		
+	
+	@Autowired
+	private KyselyRepository kyselyRepository;
+	
+	@Autowired
+	private KysymysRepository kysymysRepository;
+	
 	@RequestMapping("*")
 	public String hello() {
 		
 		return "Hello";
 	}
+	
+	@RequestMapping("/api/kyselyt") 
+	public @ResponseBody List<Kysely> getAllKyselyREST(){
+		return (List<Kysely>) kyselyRepository.findAll();
+	}
 
+	@RequestMapping("/api/kysymys")
+	public @ResponseBody List<Kysymys> getAllKysymyksetREST(){
+		return (List<Kysymys>) kysymysRepository.findAll();
+	}
 }
