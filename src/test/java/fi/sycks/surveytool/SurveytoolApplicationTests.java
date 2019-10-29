6c9b91c7eff0e776bc211da35057d0db1653a594
Sami Jaanu <jaanusa.sj@gmail.com>
2019-10-29 13:44:29 +0200

    smoke testi lisätty

--- src/test/java/fi/sycks/surveytool/SurveytoolApplicationTests.java
+++ src/test/java/fi/sycks/surveytool/SurveytoolApplicationTests.java
@@ -1,13 +1,31 @@
 package fi.sycks.surveytool;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import fi.sycks.surveytool.web.SurveyController;
+
+
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SurveytoolApplicationTests {
+public class SurveytoolApplicationTests {
+	
+	@Autowired
+	private SurveyController controller;
+	
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
+
+
+
