56ca16d555e4e749015b8cb04eb0e9e835784be3
tuoamas <tuomas.koskipahta@gmail.com>
2019-10-29 14:53:46 +0200

    Added userServiceImpl and fixes to pom

--- src/main/java/fi/sycks/surveytool/WebSecurityConfig.java
+++ src/main/java/fi/sycks/surveytool/WebSecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
+import fi.sycks.surveytool.web.UserDetailServiceImpl;
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
